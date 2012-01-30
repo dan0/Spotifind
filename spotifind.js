9abce28dbc65b633f603bbbf7e5e285663f4f1e4
ddrayne <ddrayne@spotify.com>
2012-01-30 11:53:09 +0100

    now performing search, handling results

--- spotifind.js
+++ spotifind.js
@@ -6,7 +6,7 @@ function spotifind () {
 
 	// we're only looking for tracks here
 	var baseUrl = 'http://ws.spotify.com/search/1/track.json?q=';
-
+	var foundSongs = 0;
 
 	function writeOut(msg) {
 		$('#SP-findelement').html(msg);
@@ -56,7 +56,7 @@ function spotifind () {
 	 */
 	function chooseElement($el) {
 		var selector = getElementCSSPath($el[0]);
-		var foundMessage = '<p>Search for songs on parts of the page like this?</p>';
+		var foundMessage = '<p>Search for songs?</p>';
 
 		$('#SP-findelement').html(foundMessage);
 		$('<button>Find Songs</button>').click(function() {
@@ -73,8 +73,42 @@ function spotifind () {
 		});
 	}
 
+	/**
+	 * Perform search, handle results
+	 * @param  {string} selector Css selector for searching
+	 */
 	function startSearch(selector) {
-		alert('searching for ' + selector);
+		var resultsHolder = $('#SP-findelement');
+		var results = $('<textarea id="SP-results"></textarea>');
+		var explain = $('<br><small>Copy and paste the above links into a new playlist in Spotify, and you\'re done!</small>');
+		results.appendTo(resultsHolder);
+		explain.appendTo(resultsHolder);
+
+		$(selector).each(function() {
+			// Trim and url-encode the text within our selected element
+			var searchData = encodeURIComponent($(this).text().replace(/^\s+|\s+$/g, ''));
+			var searchURL = baseUrl + searchData;
+			var foundTrack = false;
+			var el = $(this);
+
+			$.getJSON(searchURL, function(data) {
+				// quote naive, just grab the top result
+				// Without thinking about country availability - TODO: take this into account
+				if (data.tracks.length > 0) {
+					foundTrack = data.tracks[0];
+					$('<a>play</a>')
+						.css({
+							'color' : 'green',
+							'font-size' : '10px',
+							'padding-left' : '10px'
+						})
+						.attr('href', foundTrack.href)
+						.appendTo(el);
+					results.append(foundTrack.href + ' ');
+					console.log(foundTrack);
+				}
+			});
+		});
 	}
 
 	var outputDebug = '<div id="SP-findelement">' +
@@ -116,7 +150,6 @@ function spotifind () {
 
 // getScript()
 // more or less stolen form jquery core and adapted by paul irish
-
 function getScript(url,success){
 
 	var head = document.getElementsByTagName("head")[0], done = false;
